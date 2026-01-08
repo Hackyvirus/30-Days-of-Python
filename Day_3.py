98acd79821e80b3ea78eaccacbab319fc8eae538
Hackyvirus <sushantatram591081@gmail.com>
2026-01-08 07:54:10 +0530

    3rd Quesion solved

--- Day_3.py
+++ Day_3.py
@@ -1,2 +1,20 @@
 # 3. Check type of a variable
 
+A = 1
+print(f"A: {type(A)}")
+B = 1.2
+print(f"B: {type(B)}")
+C = "C"
+print(f"C: {type(C)}")
+D = "String"
+print(f"D: {type(D)}")
+E =  {1,2,3,4}
+print(f"E: {type(E)}")
+F = [1,2,3,4]
+print(f"F: {type(F)}")
+G = {"a": 1,"B":2}
+print(f"G: {type(G)}")
+
+def Function():
+    print("hello")
+print(f"Function: {type(Function)}")
