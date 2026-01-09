53d4b0f3307bcddead9c99f79ab25a4d4fe656af
Hackyvirus <sushantatram591081@gmail.com>
2026-01-09 07:53:27 +0530

    6th question Completed

---
+++ 6.py
@@ -0,0 +1,8 @@
+# 6. Add, subtract, multiply, divide two numbers
+
+A, B = map(int,input("Enter Two Numbers: ").split())
+
+print(f"A + B = {A+B}")
+print(f"A - B = {A-B}")
+print(f"A * B = {A*B}")
+print(f"A / B = {A/B}")
