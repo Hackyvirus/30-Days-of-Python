d9a44ef25c1cbd18e37415f8802bbccd1010f481
Hackyvirus <sushantatram591081@gmail.com>
2026-01-10 21:27:56 +0530

    15th question completed

---
+++ 15.py
@@ -0,0 +1,8 @@
+# 15. Check voting eligibility
+
+Age = int(input("Enter Your Age: "))
+
+if Age >= 18:
+    print(f"You are Eligible for Voting, Because Your age is {Age}")
+else:
+    print(f"You are not Eligible for Voting, Because Your age is {Age}")
