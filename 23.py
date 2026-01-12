fa9d479019b2f7393f196be2e4ba30d22bf3ae9f
Hackyvirus <sushantatram591081@gmail.com>
2026-01-12 21:11:01 +0530

    23th question completed

---
+++ 23.py
@@ -0,0 +1,14 @@
+# 23. Palindrome number
+
+number = int(input("Enter the Number: "))
+temp = number
+r = 0
+while temp > 0:
+    r *= 10
+    r += temp%10
+    temp //= 10 
+
+if r == number:
+    print(f"{number} is Palindrone")
+else:
+    print(f"{number} is not Palindrone")
